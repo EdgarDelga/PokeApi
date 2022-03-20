0eaf5b328fb8429d92f2630565dc766047e6cf23
Edgar <edgardelgadillo319@aragon.unam.mx>
2022-03-20 13:00:34 -0600

    ola

--- principal.js
+++ principal.js
@@ -17,6 +17,12 @@ fetch(url)
     document.getElementById("type").innerHTML = "Tipo: " + data.types.map((type) => type.type.name);
     document.getElementById("HP").innerHTML = "HP: " + data.stats[0].base_stat;
     document.getElementById("xp").innerHTML = data.base_experience + " exp";
+    document.getElementById("moves").innerHTML =" Moves: " + data.moves[0].move.name;
+    document.getElementById("atk").innerHTML = "ATK:     " + data.stats[2].base_stat;
+    document.getElementById("DEF").innerHTML = "DEF:     " + data.stats[3].base_stat;
+    document.getElementById("SPD").innerHTML = "SPD:     " + data.stats[5].base_stat;
+    document.getElementById("special").innerHTML = "Special ATK:     " + data.stats[4].base_stat;
+    console.log(atk)
 
     
     console.log(data)
